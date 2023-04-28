985812deb4da9b5fec3410fb38e5378f166385fe
Ayandayo Tolu Ayantunde <tolu@crowdbotics.com>
2023-04-28 16:01:58 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "missingtemtoluay12--79691",
-  url: "https://missingtemtoluay12--79691.botics.co",
-  api: "https://missingtemtoluay12--79691.botics.co/api/v1"
-}
+  "name": "missingtemtoluay12--79691",
+  "url": "https://missingtemtoluay12--79691.botics.co",
+  "api": "https://missingtemtoluay12--79691.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
